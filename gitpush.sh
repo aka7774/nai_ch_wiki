0e867d3eb4cb54bea35b5f8618834c1e7143fd70
aka7774 <aka7774@users.noreply.github.com>
2024-07-03 05:58:00 +0900

    Backup 20240703

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,7 @@
 cd /root/nai_ch_wiki
 venv/bin/python seesawiki_back_up/crawl.py backup
+today=$(date "+%Y%m%d")
 git add .
-git commit -m "Backup %date:~0,4%%date:~5,2%%date:~8,2%"
+git commit -m "Backup ${today}"
 git push
 
